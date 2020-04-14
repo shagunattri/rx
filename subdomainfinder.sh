ec29518c3e6bb02e28d9dfa33f34d8645bd984b4
shagunattri <shagunattri17@gmail.com>
2020-04-14 11:28:08 +0530

    additional tools

--- subdomainfinder.sh
+++ subdomainfinder.sh
@@ -7,11 +7,21 @@ url=$1
 if [ ! -d "$url" ];then
 	mkdir $url
 fi
-
 if [ ! -d "$url/recon" ];then
 	mkdir $url/recon
 fi
-
+if [ ! -d "$url/recon/scans" ];then
+    mkdir $url/recon/scans
+fi
+if [ ! -d "$url/recon/httprobe" ];then
+    mkdir $url/recon/httprobe
+fi
+if [ ! -d "$url/recon/potential_takeovers" ];then
+    mkdir $url/recon/potential_takeovers
+fi
+if [ ! -f "$url/recon/httprobe/alive.txt" ];then
+    touch $url/recon/httprobe/alive.txt
+fi
 
 
 echo "[+] Harvesting Subdomains with assetfinder..."
@@ -20,20 +30,24 @@ cat $url/recon/assets.txt | grep $1 >> $url/recon/subdomains.txt
 rm $url/recon/assets.txt
 echo "[+] Subdomains of $1 are harvested with assetfinder"
 
-echo " "
-
 echo "[+] Harvesting Subdomains with OWASP Amass..."
 amass enum -d $url >> $url/recon/assets.txt
 sort -u $url/recon/assets.txt >> $url/recon/subdomains.txt
 rm $url/recon/assets.txt
 echo "[+] Subdomains of $1 are harvested with OWASP Amass"
 
-echo " "
 
 echo "[+] Probing for alive domains and sorting the final result "
-
 cat $url/recon/subdomains.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/alivedomains.txt
 
+
+echo "[+] Checking for possible subdomain takeover..."
+if [ ! -f "$url/recon/potential_takeovers/potential_takeovers.txt" ];then
+    touch $url/recon/potential_takeovers/potential_takeovers.txt
+fi
+
+echo "[+] Scanning for open ports..."
+nmap -iL $url/recon/httprobe/alive.txt -T4 -oA $url/recon/scans/scanned.txt
 
 sleep 1
 
