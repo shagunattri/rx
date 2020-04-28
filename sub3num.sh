c551a32a8db7e151a2fb7ccedb3b95484fdf1087
shagunattri <shagunattri17@gmail.com>
2020-04-28 22:35:51 +0530

    OWASP Amass

--- sub3num.sh
+++ sub3num.sh
@@ -16,3 +16,8 @@ echo "[+] Harvesting Subdomains with assetfinder..."
 assetfinder $url >> $url/recon/subs.txt
 cat $url/recon/subs.txt | grep $1 >> $url/recon/final.txt
 rm $url/recon/subs.txt
+
+echo "[+] Harvesting Subdomains with OWASP Amass..."
+amass enum -d $url >> $url/recon/amass.txt
+sort -u $url/recon/amass.txt >> $url/recon/amass-result.txt
+rm $url/recon/amass.txt
