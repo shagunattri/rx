c19ff7094e3afd3690d14cc0484ec68f8d19deda
shagunattri <shagunattri17@gmail.com>
2020-04-29 18:34:14 +0530

    optimise and add nmap scan for open ports

--- sub3num.sh
+++ sub3num.sh
@@ -2,6 +2,24 @@
 
 url=$1
 
+if [ ! -x "$(command -v assetfinder)" ]; then
+        echo "[-] assetfinder required to run script"
+        exit 1
+    fi
+
+if [ ! -x "$(command -v amass)" ]; then
+        echo "[-] amass required to run script"
+        exit 1
+    fi
+if [ ! -x "$(command -v httprobe)" ]; then
+        echo "[-] httprobe required to run script"
+        exit 1
+    fi
+if [ ! -x "$(command -v nmap)" ]; then
+        echo "[-] nmap required to run script"
+        exit 1
+    fi
+
 
 if [ ! -d "$url" ];then
 	mkdir $url
@@ -9,20 +27,37 @@ fi
 if [ ! -d "$url/recon" ];then
 	mkdir $url/recon
 fi
+if [ ! -d "$url/recon/assetfinder" ];then
+	mkdir $url/recon/assetfinder
+fi
+if [ ! -d "$url/recon/amass" ];then
+	mkdir $url/recon/amass
+fi
+if [ ! -d "$url/recon/httprobe" ];then
+	mkdir $url/recon/httprobe
+fi
+if [ ! -d "$url/recon/nmap" ];then
+	mkdir $url/recon/nmap
+fi
 
 
 
-echo "[+] Harvesting Subdomains with assetfinder..."
-assetfinder $url >> $url/recon/assetfinder.txt
-cat $url/recon/assetfinder.txt | grep $1 >> $url/recon/assetfinder-final.txt
-rm $url/recon/assetfinder.txt
 
-echo "[+] Harvesting Subdomains with OWASP Amass..."
-amass enum -d $url >> $url/recon/amass.txt
-sort -u $url/recon/amass.txt >> $url/recon/amass-final.txt
-rm $url/recon/amass.txt
+# echo "[+] Harvesting Subdomains with assetfinder..."
+# assetfinder $url >> $url/recon/assetfinder/assetfinder.txt
+# cat $url/recon/assetfinder/assetfinder.txt | grep $1 >> $url/recon/assetfinder/assetfinder-final.txt
+# rm $url/recon/assetfinder/assetfinder.txt
+
+# echo "[+] Harvesting Subdomains with OWASP Amass..."
+# amass enum -d $url >> $url/recon/amass/amass.txt
+# sort -u $url/recon/amass/amass.txt >> $url/recon/amass/amass-final.txt
+# rm $url/recon/amass/amass.txt
 
 
-echo "[+] Probing for alive domains and sorting the final result..."
-cat $url/recon/assetfinder-final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe-alivedomains.txt
-cat $url/recon/amass-final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe-alivedomains.txt
+# echo "[+] Probing for alive domains and sorting the final result..."
+# cat $url/recon/assetfinder/assetfinder-final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe/httprobe-alivedomains.txt
+# cat $url/recon/amass/amass-final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe/httprobe-alivedomains.txt
+
+
+echo "[+] Scanning for open ports..."
+nmap -iL $url/recon/httprobe/httprobe-alivedomains.txt -T4 -oA $url/recon/nmap/nmap-scan.txt
