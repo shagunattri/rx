479c087b0c630379e06f61c64aeb83d1af86ebf9
shagunattri <shagunattri17@gmail.com>
2020-04-29 12:57:45 +0530

    httprobe

--- sub3num.sh
+++ sub3num.sh
@@ -13,11 +13,16 @@ fi
 
 
 echo "[+] Harvesting Subdomains with assetfinder..."
-assetfinder $url >> $url/recon/subs.txt
-cat $url/recon/subs.txt | grep $1 >> $url/recon/final.txt
-rm $url/recon/subs.txt
+assetfinder $url >> $url/recon/assetfinder.txt
+cat $url/recon/assetfinder.txt | grep $1 >> $url/recon/assetfinder-final.txt
+rm $url/recon/assetfinder.txt
 
 echo "[+] Harvesting Subdomains with OWASP Amass..."
 amass enum -d $url >> $url/recon/amass.txt
-sort -u $url/recon/amass.txt >> $url/recon/amass-result.txt
+sort -u $url/recon/amass.txt >> $url/recon/amass-final.txt
 rm $url/recon/amass.txt
+
+
+echo "[+] Probing for alive domains and sorting the final result..."
+cat $url/recon/assetfinder-final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe-alivedomains.txt
+cat $url/recon/amass-final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe-alivedomains.txt
