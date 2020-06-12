7c032ef454982c24e2e735de94c2e814297823ae
shagunattri <shagunattri17@gmail.com>
2020-06-12 18:13:51 +0530

    update and optimize

--- sub3num.sh
+++ sub3num.sh
@@ -7,8 +7,12 @@ if [ ! -x "$(command -v assetfinder)" ]; then
         echo "[-] assetfinder required to run script"
         exit 1
     fi
-if [ ! -x "$(command -v amass)" ]; then
-        echo "[-] amass required to run script"
+if [ ! -x "$(command -v meg)" ]; then
+        echo "[-] meg required to run script"
+        exit 1
+    fi
+if [ ! -x "$(command -v waybackurls)" ]; then
+        echo "[-] waybackurls required to run script"
         exit 1
     fi
 if [ ! -x "$(command -v httprobe)" ]; then
@@ -27,39 +31,57 @@ if [ ! -x "$(command -v subjack)" ]; then
         echo "[-] subjack required to run script"
         exit 1
 fi
+if [ ! -x "$(command -v html-tool)" ]; then
+        echo "[-] html-tool required to run script"
+        exit 1
+    fi
 if [ ! -d "$url" ];then
-	mkdir -p $url/recon/{assetfinder,amass,httprobe,nmap,gowitness,potential_takeovers}
+	mkdir -p $url/recon/{assetfinder,httprobe,nmap,gowitness,meg}
 fi
 
 
 
 echo "[+] Harvesting Subdomains with assetfinder..."
-assetfinder $url >> $url/recon/assetfinder/assetfinder.txt
-cat $url/recon/assetfinder/assetfinder.txt | grep $1 >> $url/recon/assetfinder/assetfinder-final.txt
-rm $url/recon/assetfinder/assetfinder.txt
+assetfinder --subs-only $url >> $url/recon/assetfinder/domains
+cat $url/recon/assetfinder/domains | grep $1 >> $url/recon/assetfinder/Domains
+rm $url/recon/assetfinder/domains
 
-echo "[+] Harvesting Subdomains with OWASP Amass..."
-amass enum -d $url >> $url/recon/amass/amass.txt
-sort -u $url/recon/amass/amass.txt >> $url/recon/amass/amass-final.txt
-rm $url/recon/amass/amass.txt
+echo "[+] Probing for live domains with httprobe..."
+cat $url/recon/assetfinder/Domains | sort -u | httprobe -s -p https:443 -c 50 | sed 's/https\?:\/\///' | tr -d ':443' |tee -a  $url/recon/httprobe/hosts
 
+echo "[+] Details of subdomains and data about(http_code + size + url)"
+cat $url/recon/assetfinder/Domains | parallel -j50 -q curl -w 'Status:%{http_code}\t  Size:%{size_download}\t %{url_effective}\n' -o /dev/null -sk | tee -a $url/recon/Domain_http_code + size + url
 
-echo "[+] Probing for alive domains and sorting the final result..."
-cat $url/recon/assetfinder/assetfinder-final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe/httprobe-alivedomains.txt
-cat $url/recon/amass/amass-final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe/httprobe-alivedomains.txt
+# echo "[+] Looking for paths using meg..."
+# cd $url/recon/httprobe/hosts | meg -v -d 1000 -c 50 /
 
+echo "[+] Types of files..."
+find . -type f 
 
-echo "[+] Scanning for open ports..."
-nmap -iL $url/recon/httprobe/httprobe-alivedomains.txt -T4 -oA $url/recon/nmap/nmap-scan.txt
+echo "[+] Looking at src tags,attributes using html-tool..."
+find . -type f | html-tool attribs src | tee -a $url/recon/html-attribs
+
+echo "[+] GREPing 200 Status from requests..."
+grep -Hlnri '200' | grep -v ^index | xargs -n1 ls -la | tee -a $url/recon/grep200 # list out 200 OK status
+
+echo "[+] Total word count of files"
+find . -type f | wc # word count
 
 echo "[+] Runnig gowitness against all compiled domains..."
-gowitness file -s $url/recon/httprobe/httprobe-alivedomains.txt -d $url/recon/gowitness
+gowitness file -s $url/recon/httprobe/hosts -d $url/recon/gowitness
 
 echo "[+] Checking for possible subdomain takeover..."
-if [ ! -f "$url/recon/potential_takeovers/potential_takeovers.txt" ];then
-    touch $url/recon/potential_takeovers/potential_takeovers.txt
+if [ ! -f "$url/recon/potential_takeovers" ];then
+    touch $url/recon/potential_takeovers
 fi
-subjack -w $url/recon/assetfinder/assetfinder-final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/recon/potential_takeovers/potential_takeovers.txt
+subjack -w $url/recon/assetfinder/Domains -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/recon/potential_takeovers
 
+echo "[+] Looking up waybackurls for $1"
+echo $1 | waybackurls | head | tee -a $url/recon/waybackurls
 
+#shoutout to tomnomnom
+echo "[+] Looking at GitHub repo's commit history dump"
+{ find .git/objects/pack/ -name "*.idx"|while read i;do git show-index < "$i"|awk '{print $2}';done;find .git/objects/ -type f|grep -v '/pack/'|awk -F'/' '{print $(NF-1)$NF}'; }|while read o;do git cat-file -p $o;done | tee -a $url/recon/ghdump
 
+echo "[+] Scanning for open ports..."
+nmap -iL $url/recon/assetfinder/Domains -T5 -oA $url/recon/nmap/nmap-scan
