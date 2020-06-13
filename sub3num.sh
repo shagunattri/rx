73fcbe2e41ba02fe0baad93be39763d02537406f
shagunattri <shagunattri17@gmail.com>
2020-06-13 23:33:21 +0530

    update checks and dirs

--- sub3num.sh
+++ sub3num.sh
@@ -3,33 +3,41 @@ set -euo pipefail
 
 url=$1
 
-if [ ! -x "$(command -v assetfinder)" ]; then
-        echo "[-] assetfinder required to run script"
+#check for tool
+if [ ! -x "$(command -v nikto)" ]; then
+        echo "[-] nikto required to run script"
         exit 1
     fi
-if [ ! -x "$(command -v waybackurls)" ]; then
-        echo "[-] waybackurls required to run script"
+if [ ! -x "$(command -v assetfinder)" ]; then
+        echo "[-] assetfinder required to run script"
         exit 1
     fi
 if [ ! -x "$(command -v httprobe)" ]; then
         echo "[-] httprobe required to run script"
         exit 1
     fi
-if [ ! -x "$(command -v subjack)" ]; then
-        echo "[-] subjack required to run script"
-        exit 1
-fi
 if [ ! -x "$(command -v html-tool)" ]; then
         echo "[-] html-tool required to run script"
         exit 1
     fi
-if [ ! -x "$(command -v nikto)" ]; then
-        echo "[-] nikto required to run script"
+if [ ! -x "$(command -v subjack)" ]; then
+        echo "[-] subjack required to run script"
+        exit 1
+fi
+if [ ! -x "$(command -v waybackurls)" ]; then
+        echo "[-] waybackurls required to run script"
         exit 1
     fi
+if [ ! -x "$(command -v gobuster)" ]; then
+        echo "[-] gobuster required to run script"
+        exit 1
+    fi
+
+#mkdir directories
 if [ ! -d "$url" ];then
-	mkdir -p $url/recon/{assetfinder,httprobe,nikto}
+	mkdir -p $url/recon/{assetfinder,httprobe,nikto,gobuster}
 fi
+
 
 echo "[+] Scanning $url web server with nikto..."
 nikto -h $url | tee -a $url/recon/nikto/serverscan
@@ -61,4 +69,4 @@ echo "[+] Looking up waybackurls for $url"
 echo $url | waybackurls | head | tee -a $url/recon/waybackurls
 
 echo "[+] Directory Bruteforcing using gobuster..."
-gobuster dir  -e -u whhc.in -w wordlists/directory-list-2.3-small.txt 
+gobuster dir  -e -u whhc.in -w wordlists/directory-list-2.3-small.txt | tee -a $url/recon/gobuster/dirbust
